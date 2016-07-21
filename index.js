cc200d1740ec8cacacae28e1e767f6f0feafa9be
rahmona-henry <rahmona.henry@icloud.com>
2016-07-21 18:30:41 +1200

    Start form stylying

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express')
 var app = express()
 
+app.use(express.static('client'))
+
 
 app.get('/', function(req,res) {
-  res.send('Hello Queenstown')
+  res.send('index.html')
 })
 
 app.listen(3000, function(){
